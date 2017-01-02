c15eb12d3a7eb7398198169aecc932ec38076d13
windcode <yangyingming2013@126.com>
2017-01-02 18:08:04 +0800

    update renewremind.py

--- renewremind.py
+++ renewremind.py
@@ -6,8 +6,6 @@ import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
 
-### 公共区
-
 timeout = 30                    # 超时时间
 charset = 'utf-8'		# 请求页面的编码格式
 subject = '【更新提示】'	# email 中的主题
@@ -58,25 +56,27 @@ def Init():
 
     fp.close()
 
-### 扩展区
-def Comic_banshoushaonian():
+
+def RenewCheck(key,src_url,des_url,pattern_str,charset):
+    # 参数介绍 :
+    # key - 检查对象，例如：西部世界、扳手少年等
+    # src_url - 检查对象的网站地址
+    # des_url - 如果有更新，提示中所指向的跳转地址
+    # pattern_str - 匹配正则表达式
+    # charset - 检查对象网站的编码
     global subject,content,isRenew,renew_dict
-    key = '扳手少年'
-    url = 'http://ac.qq.com/Comic/ComicInfo/id/520794'
-    host = 'http://ac.qq.com'
-    html = get_html(url,timeout).decode('utf-8')    # 获得页面源码
+    host = 'http://'+src_url.split('//')[1].split('/')[0]   # 检查网站的host地址
+    html = get_html(src_url,timeout).decode(charset)        # 获得页面源码
 
     # 提示信息
     print '正在检查【%s】的更新状态……'%(key)
 
     # 解析源码
-    pattern = re.compile(r'<a class="works-ft-new" href="(.*?)">(.*?)</a><span.*?>(.*?)</span>',re.S)
+    pattern = re.compile(pattern_str,re.S)
     items = re.findall(pattern,html)
 
     # 输出解析结果
-    for href,title,date in items:
-        href = host + href
-        title = title.strip()
+    title = items[0].strip()
 
     # 判断是否有更新
     cur = title.encode('utf8')
@@ -97,53 +97,11 @@ def Comic_banshoushaonian():
 
         print '【%s】有更新，发送邮件……'%(key)
         subject += '%s '%(key)
-        content += '【%s】已经更新到【%s】，戳这里看详情：%s<br/>'%(key,cur,href.encode('utf8'))
+        content += '【%s】已经更新到【%s】，戳这里看详情：%s<br/>'%(key,cur,des_url)
     else:
         # 没有更新
         print '【%s】没有更新'%(key)
 
-def Novel_douzhankuangchao():
-    global subject,content,isRenew,renew_dict
-    key = '斗战狂潮'
-    url = 'http://www.qidian.com/Book/1003694333.aspx'
-    host = 'http://www.qidian.com'
-    html = get_html(url,timeout).decode('utf-8')    # 获得页面源码
-
-    # 提示信息
-    print '正在检查【%s】的更新状态……'%(key)
-
-    # 解析源码
-    pattern = re.compile(r'</b><a class="blue" href=".*?" data-eid="qd_G19" data-cid=".*?" title=".*?" target="_blank">(.*?)</a><i>.*?</i><em class="time">(.*?)</em>',re.S)
-    items = re.findall(pattern,html)
-
-    # 输出解析结果
-    for title,date in items:
-        title = title.strip()
-
-    # 判断是否有更新
-    cur = title.encode('utf8')
-    if renew_dict.has_key(key): # 判断之前有无记录
-        last = renew_dict[key]
-    else:
-        last = None
-    if cur != last or last==None:
-        # 如果有更新，发送邮件提示
-        isRenew = True
-
-        # 更新记录
-        renew_dict[key] = cur
-        fp = open(record_file,'w')
-        for item,value in renew_dict.items():
-            fp.write('%s %s\n'%(item,value))
-        fp.close()
-
-        print '【%s】有更新，发送邮件……'%(key)
-        show_url = 'http://www.booktxt.net/2_2322/'     # 顶点小说网的链接
-        subject += '%s '%(key)
-        content += '【%s】已经更新到【%s】，戳这里看详情：%s<br/>'%(key,cur,show_url)
-    else:
-        # 没有更新
-        print '【%s】没有更新'%(key)
 
 def main():
     global subject,content,isRenew
@@ -153,11 +111,32 @@ def main():
     Init()
 
     # 检查所有更新，并输出提示信息
-    Comic_banshoushaonian()	# 漫画：扳手少年
-    Novel_douzhankuangchao()	# 小说：斗战狂潮
+    # 函数原型：
+    # def RenewCheck( key,src_url,des_url,pattern_str,charset )
+    RenewCheck('扳手少年',\
+            'http://ac.qq.com/Comic/ComicInfo/id/520794',\
+            'http://ac.qq.com/ComicView/index/id/520794/cid/176',\
+            r'<a class="works-ft-new" href=".*?">(.*?)</a><span.*?>.*?</span>',\
+            'utf8'\
+            )	# 漫画：扳手少年
+
+    RenewCheck('斗战狂潮',\
+            'http://www.qidian.com/Book/1003694333.aspx',\
+            'http://www.qidian.com',\
+            r'</b><a class="blue" href=".*?" data-eid="qd_G19" data-cid=".*?" title=".*?" target="_blank">(.*?)</a><i>.*?</i><em class="time">.*?</em>',\
+            'utf8'\
+            )   # 小说：斗战狂潮
+
+    RenewCheck('雪鹰领主',\
+            'http://www.qidian.com/Book/1003694333.aspx',\
+            'http://www.qidian.com',\
+            r'</b><a class="blue" href=".*?" data-eid="qd_G19" data-cid=".*?" title=".*?" target="_blank">(.*?)</a><i>.*?</i><em class="time">.*?</em>',\
+            'utf8'\
+            )   # 小说：斗战狂潮
 
     if isRenew:
         send_email(subject+'有更新！',content)
+
 
 if __name__ == '__main__':
     main()
